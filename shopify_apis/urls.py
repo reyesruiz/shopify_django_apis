b645a79168a30538580f3504407866c5e26020c1
Reyes Ruiz <reyes@digitalruiz.com>
2023-01-15 16:05:37 -0800

    Adding products and product data apis

--- shopify_apis/urls.py
+++ shopify_apis/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
         path('', views.index, name='index'),
         path('test', views.test, name='test'),
+        path('products', views.products, name='products'),
+        path('product', views.product, name='product'),
+        path('product/', views.product, name='product'),
+        path('product/<str:id>', views.product, name='product'),
 ]
--- shopify_apis/views.py
+++ shopify_apis/views.py
@@ -2,6 +2,8 @@
 views
 '''
 from django.http import HttpResponse
+from django.http import JsonResponse
+from com_digitalruiz_shopify_apis import shopify_apis as shopify
 
 def index(request):
     '''
@@ -16,3 +18,19 @@ def test(request):
     '''
     print(request)
     return HttpResponse("This is a test")
+
+def products(request):
+    '''
+    api to get all products
+    '''
+    print(request)
+    products = shopify.get_all_products()
+    return JsonResponse(products, safe=False)
+
+def product(request, id=0):
+    if id == 0 or not id:
+        return HttpResponse("Please provide a product id in the url")
+    product_id = id
+    shopify_product_data = shopify.get_shopify_product_data(product_id)
+    if shopify_product_data:
+        return JsonResponse(shopify_product_data, safe=False)
