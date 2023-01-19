a3b746cbe7c22f4c423ba46657032153cf6e2a65
Reyes Ruiz <reyes@digitalruiz.com>
2023-01-18 16:35:15 -0800

    Working app

--- shopify_apis/views.py
+++ shopify_apis/views.py
@@ -5,6 +5,7 @@ import sys
 from django.http import HttpResponse
 from django.http import JsonResponse
 from com_digitalruiz_my_logger import my_logger
+from com_digitalruiz_shopify_tools import shopify_tools
 from com_digitalruiz_shopify_apis import shopify_apis as shopify
 
 LOGGER = my_logger.set_logger(module_name=sys.argv[0], loglevel='INFO')
@@ -42,3 +43,24 @@ def product(request, product_id=0):
     if shopify_product_data:
         return JsonResponse(shopify_product_data, safe=False)
     return None
+
+def check_barcodes(request):
+    '''
+    Function to check if there are any duplicate barcodes
+    '''
+    LOGGER.info(request)
+    response = shopify_tools.check_barcodes()
+    return HttpResponse(response)
+
+def generate_barcodes(request, product_id=0):
+    '''
+    Function to generate barcodes
+    '''
+    LOGGER.info(request)
+    if product_id == 0 or not product_id:
+        return HttpResponse("Please provide a product id in the url")
+    response = shopify_tools.generate_barcodes(product_id)
+    print(response)
+    if response:
+        return JsonResponse(response)
+    return HttpResponse(status=500)
