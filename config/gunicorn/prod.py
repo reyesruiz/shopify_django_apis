afe3328b0377e45bcb1eefd548959d9fc82f1c02
Reyes Ruiz <reyes@digitalruiz.com>
2023-01-23 15:29:37 -0800

    Adding a way to reuse this application for different shopify store name

--- config/gunicorn/prod.py
+++ config/gunicorn/prod.py
@@ -9,13 +9,19 @@ wsgi_app = "shopify_django_apis.wsgi:application"
 # The number of worker processes for handling requests
 workers = multiprocessing.cpu_count() * 2 + 1
 # The socket to bind
-bind = os.environ.get('WEB_BIND')
+bind = '0.0.0.0:' + os.environ.get('SHOPIFY_APIS_PORT')
 # Write access and error info to /var/log
-accesslog = "/var/log/gunicorn/" + os.environ.get('shopify_store_name') + ".access.log"
-errorlog = "/var/log/gunicorn/" + os.environ.get('shopify_store_name') + ".error.log"
+accesslog = "/var/log/gunicorn/shopify-apis." + \
+        os.environ.get('shopify_store_name') + \
+        os.environ.get('domain_name') + ".access.log"
+errorlog = "/var/log/gunicorn/shopify-apis." + \
+        os.environ.get('shopify_store_name') + \
+        os.environ.get('domain_name') + ".error.log"
 # Redirect stdout/stderr to log file
 capture_output = True
 # PID file so you can easily fetch process ID
-pidfile = "/var/run/gunicorn/" + os.environ.get('shopify_store_name') + ".pid"
+pidfile = "/var/run/gunicorn/shopify-apis." + \
+        os.environ.get('shopify_store_name') + \
+        os.environ.get('domain_name') + ".pid"
 # Daemonize the Gunicorn process (detach & enter background)
 daemon = True
